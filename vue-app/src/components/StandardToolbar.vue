a6f6fcda9bd5375fe8f7ed1ce134fa868925f668
Metaphysiker <s.raess@me.com>
2024-08-03 10:15:41 +0200

    Add StandardToolbar.

---
+++ vue-app/src/components/StandardToolbar.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps({
+  title: {
+    required: true,
+    type: String,
+  },
+});
+
+const emits = defineEmits(["close"]);
+</script>
+
+<template>
+  <v-toolbar class="standard-toolbar">
+    <v-toolbar-title>{{ title }}</v-toolbar-title>
+    <div class="standard-toolbar-close-button">
+      <v-btn icon @click="emits('close')">
+        <v-icon size="large">mdi-close</v-icon>
+      </v-btn>
+    </div>
+  </v-toolbar>
+  <v-divider></v-divider>
+</template>
