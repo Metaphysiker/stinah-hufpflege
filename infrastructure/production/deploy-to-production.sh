aa1c4a13a719d2ade618d9e9320135c4ef4aa05b
Metaphysiker <s.raess@me.com>
2024-08-10 20:46:04 +0200

    Add deploy-to-production.sh.

--- infrastructure/production/deploy-to-production.sh
+++ infrastructure/production/deploy-to-production.sh
@@ -1,5 +1,8 @@
-ssh deploy@165.22.16.153
-
 ssh deploy@165.22.16.153 << EOF
-
+    cd /home/deploy/stinah-hufpflege
+    git stash
+    git pull origin main
+    cd /home/deploy/stinah-hufpflege/infrastructure/production
+    docker-compose down
+    docker-compose up --build -d
 EOF
