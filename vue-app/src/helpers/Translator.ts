3b39466281a2911edcb5317492978259886586cd
Metaphysiker <s.raess@me.com>
2025-01-04 20:34:25 +0100

    Add translations.

--- vue-app/src/helpers/Translator.ts
+++ vue-app/src/helpers/Translator.ts
@@ -2,6 +2,10 @@ export class Translator {
   dictionary: { [key: string]: string } = {
     userNotFound: "User wurde nicht gefunden.",
     wrongPassword: "Falsches Passwort.",
+    hoof: "Huf",
+    tooth: "Zahn",
+    hoofcare: "Hufpflege",
+    toothcare: "Zahnpflege",
   };
 
   translate(key: string): string {
