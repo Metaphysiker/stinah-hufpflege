d1d65cf24b001553f66e9c21ab30091a22affc83
Metaphysiker <s.raess@me.com>
2025-01-04 20:33:18 +0100

    Add selecter for category in App.vue.

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,16 +1,43 @@
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { treatmentCategories } from "./lists/TreatmentCategories";
+import { useTreatmentCategoryStore } from "./stores/treatmentCategoryStore";
+import { storeToRefs } from "pinia";
+import { Translator } from "./helpers/Translator";
+
+const translator = new Translator();
+const treatmentCategoryStore = useTreatmentCategoryStore();
+const { selectedTreatmentCategory } = storeToRefs(treatmentCategoryStore);
+
+onMounted(() => {
+  if (treatmentCategories.length > 0) {
+    selectedTreatmentCategory.value = treatmentCategories[0];
+  }
+});
+</script>
+
 <template>
   <v-app>
     <v-app-bar app :elevation="2">
-      <template v-slot:prepend>
-        <v-app-bar-nav-icon></v-app-bar-nav-icon>
-      </template>
-
-      <v-app-bar-title>Hufpflege</v-app-bar-title>
+      <v-container fluid class="h-100">
+        <div class="d-flex align-center h-100">
+          <div class="me-10"><v-app-bar-title>Pflege</v-app-bar-title></div>
+          <div>
+            <div style="width: 12rem" class="d-flex align-center h-100">
+              <v-select
+                v-model="selectedTreatmentCategory"
+                density="comfortable"
+                :hide-details="true"
+                single-line
+                label="Kategorie"
+                :item-title="(item) => translator.translate(item)"
+                :items="treatmentCategories"
+              ></v-select>
+            </div>
+          </div>
+        </div>
+      </v-container>
     </v-app-bar>
     <v-main> <RouterView /></v-main>
   </v-app>
 </template>
-
-<script setup lang="ts">
-//
-</script>
