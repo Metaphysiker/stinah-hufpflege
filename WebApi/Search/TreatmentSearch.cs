bb86b2d5e40475c987c1ecba6938ca538989ea6a
Metaphysiker <s.raess@me.com>
2025-01-11 21:52:34 +0100

    Add sortBy and sortOrder to TreatmentSearch.

--- WebApi/Search/TreatmentSearch.cs
+++ WebApi/Search/TreatmentSearch.cs
@@ -4,4 +4,7 @@ public class TreatmentSearch : ISearch
     public List<string> Categories { get; set; } = new List<string>();
     public int Page { get; set; } = 0;
     public int PageSize { get; set; } = 10;
+    public string? SortBy { get; set; }
+    public string? SortOrder { get; set; }
+
 }
