7c60af626a1ff46bdbd862f5cf5448de7afe2231
Metaphysiker <s.raess@me.com>
2025-01-11 19:27:29 +0100

    Update.

--- WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4869bfbda2dab44a9806e081076ab3690f67789b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e5aac409416685131ae5fccd0d0d57892e40e21")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
