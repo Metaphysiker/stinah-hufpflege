2cd3b0b9b33a8f3c71529e7ae20b00778fb37022
Metaphysiker <s.raess@me.com>
2025-01-13 21:55:52 +0100

    Add ModelCards.

---
+++ vue-app/src/components/generics/ModelCards.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts" generic="T extends IDto">
+import { ModelCardFactory } from "@/factories/ModelCardFactory";
+import { ServiceFactory } from "@/factories/ServiceFactory";
+import { IDto } from "@/interfaces/IDto";
+import { IModelController } from "@/interfaces/IModelController";
+import { ISearch } from "@/interfaces/ISearch";
+import { AxiosStatic } from "axios";
+import { inject, onBeforeMount, onMounted, Ref, ref } from "vue";
+const axios: AxiosStatic | undefined = inject("axios");
+const service: Ref<IModelController<T, ISearch> | undefined> = ref(undefined);
+const component: Ref<any | undefined> = ref(undefined);
+
+const props = defineProps({
+  interface: {
+    required: true,
+    type: String,
+  },
+});
+
+const models: Ref<T[]> = ref([]);
+
+const getModels = () => {
+  return new Promise<T[]>((resolve) => {
+    if (service.value) {
+      service.value.ReadAll().then((response) => {
+        models.value = response;
+        resolve(response);
+      });
+    }
+    resolve([]);
+  });
+};
+
+onBeforeMount(() => {
+  if (!axios) {
+    throw new Error("axios not injected");
+  }
+  service.value = ServiceFactory.createService(
+    props.interface,
+    axios
+  ) as IModelController<T, ISearch>;
+
+  component.value = ModelCardFactory.createComponent(props.interface);
+});
+
+onMounted(() => {
+  getModels();
+});
+</script>
+<template>
+  <div v-for="(model, index) of models" class="mb-3" :key="model.id">
+    <component v-model="models[index]"></component>
+  </div>
+</template>
