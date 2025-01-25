741f57f6c7fa1b4a1ddcc3f896e8f8b93b3683a0
Sandro Räss <s.raess@me.com>
2025-01-25 22:45:21 +0100

    Update deploy2

--- infrastructure/production2/deploy-to-production2.sh
+++ infrastructure/production2/deploy-to-production2.sh
@@ -1,8 +1,8 @@
 ssh deploy@165.22.16.153 << EOF
-    cd /home/deploy/stinah-pflege
+    cd /home/deploy/stinah-pflege/stinah-hufpflege
     git stash
-    git pull origin main
-    cd /home/deploy/stinah-pflege/infrastructure/production2
+    git pull
+    cd /home/deploy/stinah-pflege/stinah-hufpflege/infrastructure/production2
     docker compose down
     docker compose up --build -d
 EOF
