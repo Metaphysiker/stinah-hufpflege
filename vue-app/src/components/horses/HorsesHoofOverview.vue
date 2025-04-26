e9bfc7474525a34dc89d5a89ed8edeb5fa8a8c0f
Metaphysiker <s.raess@me.com>
2025-04-26 21:05:45 +0200

    Add HorsesHoofOverview.

---
+++ vue-app/src/components/horses/HorsesHoofOverview.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { IHorse } from "@/interfaces/IHorse";
+import HorseHoof from "./HorseHoof.vue";
+
+defineProps({
+  horses: {
+    required: true,
+    type: Object as () => IHorse[],
+  },
+});
+</script>
+<template>
+  <div class="d-flex">
+    <div v-for="horse in horses" class="w-50" :key="horse.id">
+      <div class="border ma-2 pa-2 rounded">
+        <HorseHoof :horse="horse"></HorseHoof>
+      </div>
+    </div>
+  </div>
+</template>
