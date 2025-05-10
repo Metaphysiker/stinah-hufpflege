412fc7046dd6c68791d8e10ec4617c9d54901e28
Sandro Räss <s.raess@me.com>
2025-05-10 17:41:59 +0200

    Add tasks to IHoofCheck.

--- vue-app/src/classes/HoofCheck.ts
+++ vue-app/src/classes/HoofCheck.ts
@@ -3,14 +3,22 @@ import { IHoofCheck } from "@/interfaces/IHoofCheck";
 
 export class HoofCheck implements IHoofCheck {
   frontLeft: HoofCheckStatuses;
+  frontLeftTask: string;
   frontRight: HoofCheckStatuses;
+  frontRightTask: string;
   backLeft: HoofCheckStatuses;
+  backLeftTask: string;
   backRight: HoofCheckStatuses;
+  backRightTask: string;
 
   constructor() {
     this.frontLeft = HoofCheckStatuses.Neutral;
+    this.frontLeftTask = "";
     this.frontRight = HoofCheckStatuses.Neutral;
+    this.frontRightTask = "";
     this.backLeft = HoofCheckStatuses.Neutral;
+    this.backLeftTask = "";
     this.backRight = HoofCheckStatuses.Neutral;
+    this.backRightTask = "";
   }
 }
--- vue-app/src/interfaces/IHoofCheck.ts
+++ vue-app/src/interfaces/IHoofCheck.ts
@@ -2,7 +2,11 @@ import { HoofCheckStatuses } from "@/enum/HoofCheckStates";
 
 export interface IHoofCheck {
   frontLeft: HoofCheckStatuses;
+  frontLeftTask: string;
   frontRight: HoofCheckStatuses;
+  frontRightTask: string;
   backLeft: HoofCheckStatuses;
+  backLeftTask: string;
   backRight: HoofCheckStatuses;
+  backRightTask: string;
 }
