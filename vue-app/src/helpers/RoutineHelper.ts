0ae46ed4925158ec64048f3dcf225ac33aca6b7e
Metaphysiker <s.raess@me.com>
2025-10-03 21:41:16 +0200

    Add getRoutinesInSameArea to RoutineHelper.

--- vue-app/src/helpers/RoutineHelper.ts
+++ vue-app/src/helpers/RoutineHelper.ts
@@ -1,16 +1,27 @@
 import { IRoutine } from "@/interfaces/IRoutine";
 import { DateHelper } from "./DateHelper";
+import { IHorse } from "@/interfaces/IHorse";
+import { ITreatmentCategory } from "@/interfaces/ITreatmentCategory";
 
 export class RoutineHelper {
-    dateHelper = new DateHelper();
+  dateHelper = new DateHelper();
 
-    calculateHypotheticalNextTreatmentDate(
-        routine: IRoutine,
-      ): Date | undefined {
-        if (routine.date) {
-          return this.dateHelper.addDays(routine.date, routine.rhythmInWeeks * 7);
-        }
+  calculateHypotheticalNextTreatmentDate(routine: IRoutine): Date | undefined {
+    if (routine.date) {
+      return this.dateHelper.addDays(routine.date, routine.rhythmInWeeks * 7);
+    }
 
-        return undefined;
-      }
+    return undefined;
+  }
+
+  getRoutinesInSameArea = (
+    horse: IHorse,
+    selectedTreatmentCategory: ITreatmentCategory | undefined
+  ): IRoutine[] => {
+    if (!selectedTreatmentCategory?.name) return horse.includedRoutines;
+    return horse.includedRoutines.filter(
+      (routine) =>
+        routine.treatmentCategoryName === selectedTreatmentCategory.name
+    );
+  };
 }
