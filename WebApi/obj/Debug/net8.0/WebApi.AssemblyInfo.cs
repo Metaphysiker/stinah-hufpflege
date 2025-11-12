a0f925599130ffc3cfce1d62679a3bfcd2e34f17
Metaphysiker <s.raess@me.com>
2025-11-12 11:08:44 +0100

    Update global.json

--- WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9177a2f5fc141443044ca821f16399c4657b618c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d690814f65ef02f400959d72b3716ca3e4ebd7c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
