916d33987e84ab114792acbc75adb057a91e1658
Sandro Räss <s.raess@me.com>
2026-07-15 19:26:29 +0200

    Add healthcheck and restart policies

--- WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0f925599130ffc3cfce1d62679a3bfcd2e34f17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf4bb9da5a076e5342bfbe651b69ab1a7d9580ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
